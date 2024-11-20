dfd7d9224d346546bf26d34f9fff728eec337884
arturo alvarez <arturo221355@gmail.com>
2024-11-20 13:02:06 -0600

    asd

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,20 +2,14 @@
   <CreditPage v-if="creditStore.credit" />
 
   <NoReferencePage v-else />
-
-  {{ route }}
 </template>
 
 <script setup>
-import NoReferencePage from "pages/NoReferencePage.vue";
-
-import CreditPage from "pages/CreditPage.vue";
+import { onMounted, defineAsyncComponent, onServerPrefetch } from "vue";
 
 import { useCreditStore } from "stores/credit-store";
 
 import { useRoute } from "vue-router";
-
-import { onMounted, watch } from "vue";
 
 const creditStore = useCreditStore();
 
@@ -29,11 +23,16 @@ defineProps({
   reference: String,
 });
 
-onMounted(() => {
+const NoReferencePage = defineAsyncComponent(() => import("pages/NoReferencePage.vue"));
+const CreditPage = defineAsyncComponent(() => import("pages/CreditPage.vue"));
+
+const name = async () => {
   if (route?.params?.reference || route.query?.referencia) {
     creditStore.reference = route.query?.referencia || route.params.reference;
 
-    creditStore.fetchCredit();
+    await creditStore.fetchCredit();
   }
-});
+};
+
+name();
 </script>
